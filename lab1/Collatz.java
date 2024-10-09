ad82e95acd90805757e6a7c00e5607cd3b4d570b
zhouyiqi <386346852@qq.com>
2024-10-09 15:56:51 +0800

    lab1

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -3,14 +3,13 @@
  */
 public class Collatz {
 
-    /** Buggy implementation of nextNumber! */
+    /** returns the next number of Collatz sequence  */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
+        if (n==1) return 1;
+        if (n % 2 == 0) {
+            return n/2;
         } else {
-            return n * 2;
+            return n*3+1;
         }
     }
 
